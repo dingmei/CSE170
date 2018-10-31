6f65d7ba3006072a17b732afb1e2f3db81999457
dingmei <d3gu@ucsd.edu>
2018-10-30 20:47:23 -0700

    done sign in/up html/css

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,21 @@
 
+$(document).ready(function(){
+    $("#sign-up").click(function(){
+        $("#sign-up-console").show();
+        $("#loginConsole").hide();
+    });
+
+    $("#sign-in").click(function(){
+        $("#sign-up-console").hide();
+        $("#loginConsole").show();
+    });
+});
 
 function login(){
 
     const email = document.getElementById('loginEmail').value;
     const pass = document.getElementById('loginPassword').value;
-    
+
 
     //real backend
     //firebase.auth().signInWithEmailAndPassword(email,pass).then(function(){
@@ -13,4 +24,3 @@ function login(){
 
     window.location.href="./home.html";
 }
-
